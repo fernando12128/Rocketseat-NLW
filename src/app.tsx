39c22fcd03cd82f76e0cbcc60532f62c8d01c9e3
Fernando Calixto <fernando12128>
2024-02-13 15:15:00 -0300

    Projeto finalizado

---
+++ src/app.tsx
@@ -0,0 +1,84 @@
+import { ChangeEvent, useState } from 'react'
+import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { Notecard } from './components/note-card'
+
+interface Note{
+  id:string
+  date: Date
+  content: string
+ 
+}
+
+export function App() {
+  const [search, setSearch] = useState('')
+  const [notes,setNotes] = useState<Note[]>(()=> {
+    const notesOnStorege = localStorage.getItem('notes')
+
+    if(notesOnStorege){
+      return JSON.parse(notesOnStorege)
+    }
+    
+    return []
+  })
+
+  function onNoteCreated(content: string){
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    const notesArray = [newNote,...notes]
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function onDeleted(id: string){
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>){
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredNotes = search !== '' ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())) : notes
+
+  return (
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
+      <img src = {logo} alt="nlw express"/>
+
+      <form action="" className='w-full'>
+        <input 
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500" 
+          type="text" 
+          placeholder='Busque em suas notas...' 
+          onChange={handleSearch}
+        />   
+      </form>
+
+      <div className='h-px bg-slate-700'/>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'> 
+             
+      <NewNoteCard onNoteCreated={onNoteCreated}/>
+      {filteredNotes.map(note => {
+        return <Notecard key={note.id} note={note} onDeleted={onDeleted}/>
+      })}
+
+      
+
+      </div>
+    </div>
+    
+  )
+}
+
